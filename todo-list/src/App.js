7beeeb3cc3db80ba58dca8e19a81311670fe97a8
Muhammad Usman Karim <96872170+usmannkarim@users.noreply.github.com>
2023-12-24 00:45:25 +0500

    Updated App.js

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,72 +1,83 @@
-import { useState } from "react";
 import "./App.css";
-
+import {useState} from 'react'
+ 
 function App() {
   const [list, setList] = useState([])
   const [inputText, setInputText] = useState()
-  const [buttonSwitch, setButtonSwitch] = useState(false)
-  const [currentIndex, setCurrentIndex] = useState()
+  const [buttonMode , setButtonMode] = useState (false)
+  const [currrentIndex, setCurrentIndex] = useState()
 
   function updateText(e){
     const value = e.target.value
     setInputText(value)
-  }
+  }  
 
   function addItem(){
-    if(inputText!==''){
-   const copyList = [...list]
-   copyList.push(inputText)
-   setList(copyList)
-   setInputText('')
-  }
-  else{
-    alert('Empty Task are not entered in the List')
+    if (inputText!==""){
+    const copyList = [...list]
+    copyList.push(inputText)
+    setList(copyList)
     setInputText('')
   }
-}
+  else{
+    alert('Empty Task Cannot be entered')
+    setInputText('')
+  }
+ }
 
   function deleteItem(index){
     const copyList = [...list]
     copyList.splice(index, 1)
     setList(copyList)
-    setButtonSwitch(false)
+    setButtonMode(false)
+    setInputText('')
   }
 
   function editItem(index){
+    //purpose: 
+    // 1. show the value in the list to the input
+    // 2. change the button from 'Add' to 'Update'
     const taskValue = list[index]
     setInputText(taskValue)
-    setButtonSwitch(true)
+    setButtonMode(true)
     setCurrentIndex(index)
-  }
+  } 
 
-  function updateTask(){
+  function updateItem () {
+    // purpose:
+    // list[index] = newvalue
+    // change button back to add
+    // clear the input
     const copyList = [...list]
-    copyList[currentIndex] = inputText
+    copyList[currrentIndex] = inputText
     setList(copyList)
-    setButtonSwitch(false)
+    setButtonMode(false)
     setInputText('')
+
   }
-  
 
   return (
     <div className="App">
       <header className="App-header">
         <h3>Todo-List</h3>
-        <div>
-        <input onChange={updateText} placeholder="Enter any Task" value={inputText}/>
-        {buttonSwitch ? <button onClick={updateTask}>Update Task</button> : <button onClick={addItem}>Add Task</button>}
-        
-        {list.map(function(item, index){
-          return <ul>
-          <li>{item}</li>
-          <button onClick={()=> editItem(index)}>Edit Task</button>
-          <button onClick={()=>deleteItem(index)}>Delete Task</button>
-
-
-          </ul>
-        })}
+        <div className="inputtask">
+          <input required onChange={updateText} placeholder="Enter a Task to Your List" value={inputText}></input>
         </div>
-      </header>
+        <div className="buttonsect">
+        {buttonMode ? <button onClick={updateItem}>Update Task</button> : <button onClick={addItem}>Add Task</button>  }
+        </div>
+        <ul>
+        {list.map(function(item, index){
+          return <li>
+          {item}
+          {" "}
+          <button onClick={()=> editItem(index)}>Edit Task</button>
+          {" "}
+          <button onClick={()=> deleteItem(index)}>Delete Task</button>
+          </li>
+        })}
+        </ul>
+      </header> 
     </div>
   );
 }
